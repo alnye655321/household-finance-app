2cc229bec52d10b5dc1a6864f13db03421740f06
alnye655321 <wiggles@nyedigital.com>
2021-02-08 08:57:40 -0500

    added account info

--- client/src/components/Accounts.vue
+++ client/src/components/Accounts.vue
@@ -8,27 +8,17 @@
     >
       <v-row class="fill-height">
         <v-card-title>
-          <v-btn
-              dark
-              icon
-          >
+          <v-btn dark icon>
             <v-icon>mdi-chevron-left</v-icon>
           </v-btn>
 
           <v-spacer></v-spacer>
 
-          <v-btn
-              dark
-              icon
-              class="mr-4"
-          >
+          <v-btn dark icon class="mr-4">
             <v-icon>mdi-pencil</v-icon>
           </v-btn>
 
-          <v-btn
-              dark
-              icon
-          >
+          <v-btn dark icon>
             <v-icon>mdi-dots-vertical</v-icon>
           </v-btn>
         </v-card-title>
@@ -58,6 +48,8 @@
 <!--        </v-list-item-icon>-->
       </v-list-item>
 
+      <v-divider inset></v-divider>
+
       <v-list-item>
         <v-list-item-icon>
           <v-icon color="indigo">mdi-calendar-range</v-icon>
@@ -65,44 +57,7 @@
 
         <v-list-item-content>
           <v-list-item-title>{{account.dateOpened}}</v-list-item-title>
-          <v-list-item-subtitle>Balance</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-list-item>
-        <v-list-item-action></v-list-item-action>
-
-        <v-list-item-content>
-          <v-list-item-title>(323) 555-6789</v-list-item-title>
-          <v-list-item-subtitle>Work</v-list-item-subtitle>
-        </v-list-item-content>
-
-        <v-list-item-icon>
-          <v-icon>mdi-message-text</v-icon>
-        </v-list-item-icon>
-      </v-list-item>
-
-      <v-divider inset></v-divider>
-
-      <v-list-item>
-        <v-list-item-icon>
-          <v-icon color="indigo">
-            mdi-email
-          </v-icon>
-        </v-list-item-icon>
-
-        <v-list-item-content>
-          <v-list-item-title>aliconnors@example.com</v-list-item-title>
-          <v-list-item-subtitle>Personal</v-list-item-subtitle>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-list-item>
-        <v-list-item-action></v-list-item-action>
-
-        <v-list-item-content>
-          <v-list-item-title>ali_connors@example.com</v-list-item-title>
-          <v-list-item-subtitle>Work</v-list-item-subtitle>
+          <v-list-item-subtitle>Date Opened</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
 
@@ -110,16 +65,45 @@
 
       <v-list-item>
         <v-list-item-icon>
-          <v-icon color="indigo">
-            mdi-map-marker
-          </v-icon>
+          <v-icon color="indigo">mdi-cash-multiple</v-icon>
         </v-list-item-icon>
 
         <v-list-item-content>
-          <v-list-item-title>1400 Main Street</v-list-item-title>
-          <v-list-item-subtitle>Orlando, FL 79938</v-list-item-subtitle>
+          <v-list-item-title>{{account.interestRate}}</v-list-item-title>
+          <v-list-item-subtitle>Interest Rate</v-list-item-subtitle>
         </v-list-item-content>
       </v-list-item>
+
+      <v-divider inset></v-divider>
+
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon color="indigo">mdi-cash-multiple</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{account.accountType.accountType}}</v-list-item-title>
+          <v-list-item-subtitle>Account Type</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+
+      <v-divider inset v-if="account.dateClosed"></v-divider>
+
+      <v-list-item v-if="account.dateClosed">
+        <v-list-item-icon>
+          <v-icon color="indigo">mdi-cash-multiple</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{account.dateClosed}}</v-list-item-title>
+          <v-list-item-subtitle>Date Closed</v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+
+
+
     </v-list>
   </v-card>
   </div>
